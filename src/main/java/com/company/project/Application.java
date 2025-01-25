04988dc81938d49eb85f9da5d7bce71389f12a81
lestergyl <534047406@qq.com>
2025-01-25 13:22:16 +0800

    init commit commit2 commit3

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello2");
+        System.out.println("hello3");
         SpringApplication.run(Application.class, args);
     }
 }
