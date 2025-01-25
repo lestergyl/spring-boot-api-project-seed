e563df496093dae71bc625adcda671e2746671ef
lestergyl <534047406@qq.com>
2025-01-25 13:23:03 +0800

    commit2

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
         SpringApplication.run(Application.class, args);
     }
 }
